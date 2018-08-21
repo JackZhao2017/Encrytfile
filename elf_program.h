d452713c1a51f9c1635589d6cc803ce14938b0f2
JackZhao <kobe2014zyp@126.com>
2018-08-21 14:31:22 +0800

    encrypt v1.0

--- elf_program.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ELF_PROGRAM_H_
-#define _ELF_PROGRAM_H_
-#include <elf.h>
-void program_header_Process(Elf32_Phdr *pro_header);
-void ger_programs(FILE *fp ,Elf32_Phdr *pro_header,int size);
-
-#endif
